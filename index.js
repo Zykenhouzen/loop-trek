e5b884dd5b21bae9a524f9af64b2f303d64e0c47
Zykenhouzen <peterzylka@gmail.com>
2018-10-18 23:54:34 -0500

    Fixing losing case.

--- index.js
+++ index.js
@@ -21,14 +21,16 @@ function findIt() {
 
     let newCompetingPaths = [];
     let success = false;
+    let stepsGained = true;
 
-    while(success === false) {
+    while(success === false && stepsGained === true) {
+        stepsGained = false;
         for(let competingPathIndex in competingPaths) {
             let competingPath = competingPaths[competingPathIndex];
             let currentSteps = getPossibleSteps(competingPath[competingPath.length-1], traverseMap);
-            traverseMap[competingPath[competingPath.length-1].x + ':' + competingPath[competingPath.length-1].y] = true;
             for(let currentStepIndex in currentSteps) {
                 let currentStep = currentSteps[currentStepIndex];
+                traverseMap[currentStep.x + ':' + currentStep.y] = true;
                 let newCompetingPath = JSON.parse(JSON.stringify(competingPath));
                 newCompetingPath.push(currentStep);
                 if(JSON.stringify(pCoordinate) === JSON.stringify(currentStep)) {
@@ -36,29 +38,32 @@ function findIt() {
                     console.log("Winner Winner Chicken Dinner: " + JSON.stringify(newCompetingPath));
                     return true;
                 } else if (maze[currentStep.y][currentStep.x] === 'O') {
+                    stepsGained = true;
                     newCompetingPaths.push(newCompetingPath);
                 }
             }
         }
         competingPaths = newCompetingPaths;
     }
+
+    console.log("Womp Womp. No way through.")
 }
 
 function getPossibleSteps(coordinate, traverseMap) {
     let stepsArray = [];
-    if(coordinate.x > 0 && !traverseMap[coordinate.x-1 + ':' + coordinate.y]) {
+    if(coordinate.x > 0 && !traverseMap[(coordinate.x-1) + ':' + coordinate.y]) {
         stepsArray.push(new Point(coordinate.x-1, coordinate.y));
     }
 
-    if(coordinate.x < maze[0].length-1 && !traverseMap[coordinate.x+1 + ':' + coordinate.y]) {
+    if(coordinate.x < maze[0].length-1 && !traverseMap[(coordinate.x+1) + ':' + coordinate.y]) {
         stepsArray.push(new Point(coordinate.x+1, coordinate.y));
     }
 
-    if(coordinate.y > 0 && !traverseMap[coordinate.x + ':' + coordinate.y-1]) {
+    if(coordinate.y > 0 && !traverseMap[coordinate.x + ':' + (coordinate.y-1)]) {
         stepsArray.push(new Point(coordinate.x, coordinate.y-1));
     }
 
-    if(coordinate.y < maze.length-1 && !traverseMap[coordinate.x + ':' + coordinate.y+1]) {
+    if(coordinate.y < maze.length-1 && !traverseMap[coordinate.x + ':' + (coordinate.y+1)]) {
         stepsArray.push(new Point(coordinate.x, coordinate.y+1));
     }
     return stepsArray;
